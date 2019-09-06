83948fe10cf86ca84fbdd2a66f4350fd55d40560
jitendra <jiten.cs2010@gmail.com>
2019-09-06 10:32:03 +0530

    updated calculation and cleanup

--- tax-calculate.js
+++ tax-calculate.js
@@ -5,51 +5,44 @@
 *   above that: 30%
 *   find overall tax on salary 
 */
-
 function getAbsoluteTax(income, investment80C) {
   const taxableIncome = getTaxableIncome(income, investment80C);
     if(taxableIncome<250001) {
         return (taxableIncome*5)/100;
     } else if (taxableIncome<750001) {
-        // console.log((getTaxableIncome(income)*5)/100);    
-        return (getTaxableIncome(income)*5)/100;
-    } else if(income<1000001) {
-        const taxableIncome = getTaxableIncome(income);
-        // console.log(12500+((taxableIncome-500000)*20)/100);
         return 12500+((taxableIncome-250000)*20)/100;
-    } else {
-        const taxableIncome = getTaxableIncome(income);
-        // console.log(12500+100000+((taxableIncome-1000000)*30)/100);
+    } else if(taxableIncome>750000) {
         return 12500+100000+((taxableIncome-750000)*30)/100;
     }
+    return 0;
 }
 
 function getTaxableIncome(totalIncome, investment80C) {
     if(totalIncome>250000) {
       let remainingIncome = totalIncome-250000;
       if(investment80C) {
-        if(remainingIncome >= investment80C) {
-            return totalIncome-250000-investment80C;
+        if(investment80C>=150000) {
+           return remainingIncome-150000>-1 ? remainingIncome-150000 : 0;
         } else {
-          return 0;  
-        }          
-      } 
-      return totalIncome-250000;
+          return remainingIncome-investment80C>-1 ? remainingIncome-investment80C : 0;
+        }                  
+      } else {
+        return remainingIncome;  
+      }      
     }
     return 0;
 }
 
 function getOverallTax(income, tax) {
-    return (tax/income)*100;
+    return parseInt((tax/income)*100);
 }
 
 var out = [];
 var investment80C = prompt('any investment');
-for(let i=1;i<=50;i++) {
+for(let i=1;i<=20;i+=4) {
   const income = i*100000;
   const tax = getAbsoluteTax(income, investment80C);
   const overallTax = getOverallTax(income, tax);
   out.push({income, tax, overallTax});
-  // console.table(i*100000, getAbsoluteTax(i*100000));
 }
-console.table(out)
+console.table(out);
