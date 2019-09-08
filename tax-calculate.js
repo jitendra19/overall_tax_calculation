ac9a9451fb1bdb46d6280aa99710cdd67bc79dc5
jitendra <jiten.cs2010@gmail.com>
2019-09-08 23:50:10 +0530

    updated calculation with 80TTA, 80G, HRA

--- tax-calculate.js
+++ tax-calculate.js
@@ -5,6 +5,7 @@
 *   above that: 30%
 *   find overall tax on salary 
 */
+
 function getAbsoluteTax(income, investment80C) {
   const taxableIncome = getTaxableIncome(income, investment80C);
     if(taxableIncome<250001) {
@@ -17,19 +18,28 @@ function getAbsoluteTax(income, investment80C) {
     return 0;
 }
 
-function getTaxableIncome(totalIncome, investment80C) {
+function getTaxableIncome({totalIncome, investment80C, _80G, _80TTA, HRA}) {   
+    investment80C = investment80C ? investment80C>150000 
+                                      ? 150000 : investment80C
+                                    : 0;  
+    _80G = _80G ? _80G : 0;
+    _80TTA = _80TTA ? _80TTA : 0;
+    HRA = HRA ? HRA : 0;
+
+    const totaltax =  parseInt(investment80C) + parseInt(_80G) + parseInt(_80TTA) + parseInt(HRA);
+
     if(totalIncome>250000) {
       let remainingIncome = totalIncome-250000;
-      if(investment80C) {
-        if(investment80C>=150000) {
-           return remainingIncome-150000>-1 ? remainingIncome-150000 : 0;
-        } else {
-          return remainingIncome-investment80C>-1 ? remainingIncome-investment80C : 0;
-        }                  
+      if(totaltax) {
+        // if(investment80C>=150000) {
+        //   return remainingIncome-150000>-1 ? remainingIncome-150000 : 0;
+        // } else {
+          return remainingIncome-totaltax-40000 > -1 ? remainingIncome-totaltax-40000 : 0;
+        // }                  
       } else {
         return remainingIncome;  
       }      
-    }
+   }
     return 0;
 }
 
@@ -38,11 +48,14 @@ function getOverallTax(income, tax) {
 }
 
 var out = [];
-var investment80C = prompt('any investment');
-for(let i=1;i<=20;i+=4) {
-  const income = i*100000;
-  const tax = getAbsoluteTax(income, investment80C);
-  const overallTax = getOverallTax(income, tax);
-  out.push({income, tax, overallTax});
+var investment80C = prompt('Any investment under 80C?');
+var _80G = prompt('Any donation on 100% under 80G?');
+var _80TTA = prompt('80TTA saving bank account interest max 10k?');
+var HRA = prompt('HRA on rent?');
+for(let i=1;i<=50;i++) {
+  const totalIncome = i*100000;
+  const tax = getAbsoluteTax({totalIncome, investment80C, _80G, _80TTA, HRA});
+  const overallTax = getOverallTax(totalIncome, tax);
+  out.push({totalIncome, tax, overallTax});
 }
 console.table(out);
